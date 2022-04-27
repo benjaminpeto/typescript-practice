9aa897974d294f9435f0f6822f75e1fe99fa2871
Benji <benjaminpeto23@gmail.com>
2022-04-27 15:48:37 +0200

    added type annotations for functions and variables, reconfigurated tsconfig.base.json for checking strictNullChecks and noImplicitAny set to true

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,15 @@
 function startGame() {
+  // STARTING GAME
+
+  let playerName: string = 'Capitan';
+  logPlayer(playerName);
+
   let messagesElement = document.getElementById('messages');
   messagesElement!.innerText = 'Welcome to the MathGame! Starting a new game...';
+}
+
+function logPlayer(name: string): void {
+  console.log(`New game initiated for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
